0538fa23a8c713953d9ac58c5bfb14611dc99c60
mkaan11 <mkaanacar2001@hotmail.com>
2021-11-25 05:22:54 +0300

    Generate squares in a container

--- sketch.js
+++ sketch.js
@@ -1,7 +1,26 @@
-const container = document.getElementById('container')
+// const squarecontainer = document.getElementById('squarecontainer')
 
-for (let i = 0; i < 810; i++) {
-    let square = document.createElement('div');
-    square.className = "squares";
-    container.appendChild(square);
+//square container is 600px-600px
+
+const squareNumberOnSide = document.getElementById('squareNumber').value;
+const totalSquareNumber = squareNumberOnSide * squareNumberOnSide;
+const squareWidthValue = (600 / squareNumberOnSide);
+const squareWidth = squareWidthValue.toString() + "px";
+
+
+for (let i = 0; i < totalSquareNumber; i++) {
+    const  newSquare = document.createElement('div');
+    newSquare.className = "squares";
+    // Get the css root element
+    const root = document.querySelector(':root');
+    root.style.setProperty('--squareLength', squareWidth)
+    squarecontainer.appendChild(newSquare);
 }
+
+const allSquares = document.querySelectorAll('.squares');
+
+allSquares.forEach(square => square.addEventListener('click', (e) => {e.target.classList.add('painted')} ));
+
+const clearButton = document.getElementById('clear')
+
+clearButton.addEventListener('click',() => {allSquares.forEach(square => square.classList.remove('painted'))});
