26e3dd5e7100db43153335b02818afb18169b2a2
mkaan11 <mkaanacar2001@hotmail.com>
2021-11-25 08:00:12 +0300

    Add functionality to slider button

--- sketch.js
+++ sketch.js
@@ -2,25 +2,44 @@
 
 //square container is 600px-600px
 
-const squareNumberOnSide = document.getElementById('squareNumber').value;
-const totalSquareNumber = squareNumberOnSide * squareNumberOnSide;
-const squareWidthValue = (600 / squareNumberOnSide);
-const squareWidth = squareWidthValue.toString() + "px";
 
+const squareContainer = document.getElementById('squarecontainer');
+const slider = document.getElementById('squareNumber');
 
-for (let i = 0; i < totalSquareNumber; i++) {
-    const  newSquare = document.createElement('div');
-    newSquare.className = "squares";
-    // Get the css root element
-    const root = document.querySelector(':root');
-    root.style.setProperty('--squareLength', squareWidth)
-    squarecontainer.appendChild(newSquare);
-}
-
-const allSquares = document.querySelectorAll('.squares');
-
-allSquares.forEach(square => square.addEventListener('click', (e) => {e.target.classList.add('painted')} ));
 
 const clearButton = document.getElementById('clear')
 
-clearButton.addEventListener('click',() => {allSquares.forEach(square => square.classList.remove('painted'))});
+
+function squareFiller(){
+    const squareNumberOnSide = document.getElementById('squareNumber').value;
+    const totalSquareNumber = squareNumberOnSide * squareNumberOnSide;
+    const squareWidthValue = (600 / squareNumberOnSide);
+    const squareWidth = squareWidthValue.toString() + "px";
+
+    for (let i = 0; i < totalSquareNumber; i++) {
+        const  newSquare = document.createElement('div');
+        newSquare.className = "squares";
+        // Get the css root element
+        const root = document.querySelector(':root');
+        root.style.setProperty('--squareLength', squareWidth)
+        squareContainer.appendChild(newSquare);
+
+    }
+
+    const allSquares = document.querySelectorAll('.squares');
+    allSquares.forEach(square => square.addEventListener('mouseover', (e) => {e.target.classList.add('painted')} ));
+
+
+}
+
+function squareDeleter() {
+    while (squareContainer.firstChild) {
+        squareContainer.removeChild(squareContainer.firstChild);
+    }
+
+}
+
+squareFiller();
+slider.addEventListener('change',() => { squareDeleter(), squareFiller();} );
+
+clearButton.addEventListener('click',() => {document.querySelectorAll('.squares').forEach(square => square.classList.remove('painted'))});
